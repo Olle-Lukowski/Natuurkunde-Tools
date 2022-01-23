337ee862542da026c117aea4e32a08bfbad75601
Josh <92541477+Josh007007@users.noreply.github.com>
2022-01-23 17:27:07 +0100

    Start
    I did it

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+Formulas = "Math, Economy, Phisics (0, 1, 2): "
+
+Subject = int(input(Formulas))
+
+if Subject == 0:
+    #Math
+    print("Sowy")
+elif Subject == 1:
+    #Economy
+    print("Sowy")
+elif Subject == 2:
+    #Phisics
+    print("YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    time.sleep(1)
